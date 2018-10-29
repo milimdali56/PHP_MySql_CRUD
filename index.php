cbc3b6aa796b119b7bfea65c8186ca6468de9d80
milimdali56 <milimdali@gmail.com>
2018-10-29 12:24:34 +0600

    fisrt step done

---
+++ index.php
@@ -0,0 +1,16 @@
+
+<!DOCTYPE HTML>
+<html>  
+<body>
+
+<form action="hello.php" method="get">
+
+ <input type="text" name="num1" placeholder="give first number">
+<input type="text" name="opa" placeholder="+ or - or * or /">
+<input type="text" name="num2" placeholder="give second number">
+
+<input type="submit">
+</form>
+
+</body>
+</html>
